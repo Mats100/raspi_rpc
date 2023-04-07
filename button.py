6bb068140a8c293b6e929f4673fbef8e8d70fb0e
Mats100 <muneeb75@icloud.com>
2023-04-07 17:45:23 +0500

    added new status api

--- button.py
+++ button.py
@@ -4,16 +4,25 @@ from autobahn import wamp
 
 class RelaySwitch:
 
-    @wamp.register('com.test.relay', check_types=True)
+    @wamp.register('com.pk.codebase.relay.status.set', check_types=True)
     async def switch_relay(self):
-        relay_pins = 16
+        relay_pins = 26
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(relay_pins, GPIO.OUT)
         GPIO.setwarnings(False)
-        print(GPIO.gpio_function(relay_pins))
+        GPIO.setup(relay_pins, GPIO.OUT)
         if GPIO.input(relay_pins):
             GPIO.output(relay_pins, GPIO.LOW)
         else:
             GPIO.output(relay_pins, GPIO.HIGH)
 
-        GPIO.cleanup()
+    @wamp.register('com.pk.codebase.relay.status.get', check_types=True)
+    async def relay_status(self):
+        relay_pins = 26
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
+        GPIO.setup(relay_pins, GPIO.OUT)
+
+        status = GPIO.input(relay_pins)
+        if status == 0:
+            return True
+        return False
